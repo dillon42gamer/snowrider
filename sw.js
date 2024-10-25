48b66f65ebda6c322633cdff1bfc7dd62f6b7727
dillon42gamer <160809922+dillon42gamer@users.noreply.github.com>
2024-10-24 19:00:45 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,14 @@
-// No Service Worker
-console.log("No Service Worker")
+function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen().catch(err => {
+            alert(`Error attempting to enter fullscreen mode: ${err.message}`);
+        });
+    } else {
+        document.exitFullscreen();
+    }
+}
+
+window.addEventListener("resize", () => {
+    document.getElementById('app-container').style.width = window.innerWidth + 'px';
+    document.getElementById('app-container').style.height = window.innerHeight + 'px';
+});
